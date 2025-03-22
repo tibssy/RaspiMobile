aabdc8fa3ec2e07b672da1a7ff211948685fa8f6
tibssy <norbert.tiborcz@gmail.com>
2025-03-22 22:53:54 +0000

    Add erification sent page

--- raspimobile/settings.py
+++ raspimobile/settings.py
@@ -29,6 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', False)
+# DEBUG = False
 
 
 ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
