b935ba775ba688f2c77f3d9fe32c47ae991555b4
tibssy <norbert.tiborcz@gmail.com>
2025-03-26 21:40:06 +0000

    Add postgresql to project

--- raspimobile/settings.py
+++ raspimobile/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 import os
 if os.path.isfile('env.py'):
@@ -102,10 +103,7 @@ WSGI_APPLICATION = "raspimobile.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 CSRF_TRUSTED_ORIGINS = [
