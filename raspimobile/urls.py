7aa64a5fa1ac2ad3f01f287f55a7cdc176825545
tibssy <norbert.tiborcz@gmail.com>
2025-03-31 15:09:24 +0100

    Create products app

--- raspimobile/urls.py
+++ raspimobile/urls.py
@@ -21,5 +21,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("allauth.urls")),
-    path('', include('home.urls'), name='home-urls'),
+    path('', include('home.urls')),
+    path('products/', include('products.urls')),
 ]
