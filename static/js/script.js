8fc48c3b75b4d5b8d1083f1fe38b645cfc32e011
tibssy <norbert.tiborcz@gmail.com>
2025-03-31 01:48:11 +0100

    Add carousel to sidebar

--- static/js/script.js
+++ static/js/script.js
@@ -1,99 +1,117 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const sidebar = document.querySelector('#sidebar');
     const main = document.querySelector('main');
-    const cartButton = document.querySelector('#cartButton');
-    const cartButtonMobile = document.querySelector('#cartButtonMobile');
+    const sidebar = document.querySelector('#sidebar');
     const navLinks = document.querySelectorAll('.navbar-nav .nav-link, a.navbar-brand, header .icon-button[href]');
+    const userButtons = document.querySelectorAll('.user-button');
+    const assistantButtons = document.querySelectorAll('.assistant-button');
+    const cartButtons = document.querySelectorAll('.cart-button');
+    const sidebarControls = document.querySelectorAll('.sidebar-control');
+    const sidebarCarousel = bootstrap.Carousel.getOrCreateInstance(sidebar);
+    const sidebarCarouselItems = document.querySelectorAll('#sidebar .carousel-item');
     const animationDuration = 300;
+    let isSidebarOpen = false;
 
 
-    function openSidebarDesktop() {
-        main.classList.add('sidebar-open');
-        cartButton.classList.add('active');
+    function setCarouselItemActive(index) {
+        sidebarCarouselItems.forEach(item => {
+            item.classList.remove('active');
+        });
+        sidebarCarouselItems[index].classList.add('active');
     }
 
-    function closeSidebarDesktop() {
+    function setSidebarControlActive(control) {
+        resetSidebarControls();
+        control.forEach(item => {
+            item.classList.add('active');
+        });
+    }
+
+    function resetSidebarControls() {
+        sidebarControls.forEach(button => {
+            button.classList.remove('active');
+        });
+    }
+
+    function openSidebar() {
+        if (window.innerWidth >= 768) {
+            main.classList.add('sidebar-open');
+        } else {
+            main.classList.add('move-left');
+            sidebar.classList.add('align-left');
+        }
+        isSidebarOpen = true;
+    }
+
+    function closeSidebar() {
         main.classList.remove('sidebar-open');
-        cartButton.classList.remove('active');
-    }
-
-    function openSidebarMobile() {
-        main.classList.add('move-left');
-        sidebar.classList.add('align-left');
-        cartButtonMobile.classList.add('active');
-    }
-
-    function closeSidebarMobile() {
         main.classList.remove('move-left');
         sidebar.classList.remove('align-left');
-        cartButtonMobile.classList.remove('active');
+        resetSidebarControls();
+        isSidebarOpen = false;
     }
 
-    cartButton.addEventListener('click', () => {
-        if (window.innerWidth >= 768) {
-            if (main.classList.contains('sidebar-open')) {
-                closeSidebarDesktop();
-            } else {
-                openSidebarDesktop();
-            }
+    function openUser() {
+        if (isSidebarOpen) {
+            sidebarCarousel.to(0);
         } else {
-             console.log('cart button clicked on small screen');
+            setCarouselItemActive(0);
+            openSidebar();
         }
+
+        setSidebarControlActive(userButtons);
+    }
+
+    function openAssistant() {
+        if (isSidebarOpen) {
+            sidebarCarousel.to(1);
+        } else {
+            setCarouselItemActive(1);
+            openSidebar();
+        }
+
+        setSidebarControlActive(assistantButtons);
+    }
+
+    function openCart() {
+        if (isSidebarOpen) {
+            sidebarCarousel.to(2);
+        } else {
+            setCarouselItemActive(2);
+            openSidebar();
+        }
+
+        setSidebarControlActive(cartButtons);
+    }
+
+//    Carousel Control Buttons
+    userButtons.forEach(button => {
+        button.addEventListener('click', openUser);
     });
 
-    cartButtonMobile.addEventListener('click', () => {
-         if (window.innerWidth < 768) {
-            if (main.classList.contains('move-left')) {
-                closeSidebarMobile();
-            } else {
-                openSidebarMobile();
-            }
-        } else {
-             if (main.classList.contains('sidebar-open')) {
-                closeSidebarDesktop();
-            } else {
-                openSidebarDesktop();
-            }
-        }
+    assistantButtons.forEach(button => {
+        button.addEventListener('click', openAssistant);
     });
 
+    cartButtons.forEach(button => {
+        button.addEventListener('click', openCart);
+    });
+
+//    Close Sidebar on Redirect
     navLinks.forEach(link => {
         link.addEventListener('click', (event) => {
             const destinationUrl = event.currentTarget.href;
 
-            const isSidebarOpenDesktop = main.classList.contains('sidebar-open');
-            const isSidebarOpenMobile = main.classList.contains('move-left');
-            const isSidebarOpen = isSidebarOpenDesktop || isSidebarOpenMobile;
-
             if (isSidebarOpen && destinationUrl && destinationUrl !== '#') {
                 event.preventDefault();
-
-                if (isSidebarOpenDesktop) {
-                    closeSidebarDesktop();
-                }
-                if (isSidebarOpenMobile) {
-                    closeSidebarMobile();
-                }
+                closeSidebar();
 
                 setTimeout(() => {
                     window.location.href = destinationUrl;
                 }, animationDuration);
-
-            } else {
-                console.log('Sidebar closed or or not nav link');
             }
         });
     });
 
-     window.addEventListener('resize', () => {
-         if (window.innerWidth >= 768) {
-             if (main.classList.contains('move-left')) {
-                 closeSidebarMobile();
-             }
-         } else {
-             if (main.classList.contains('sidebar-open')) {
-                 closeSidebarDesktop();
-             }
-         }
-     });
+//    Close Sidebar on Resize
+    window.addEventListener('resize', closeSidebar);
 });
