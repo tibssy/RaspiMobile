8b9ca8deaaab35fe67b75ba28098235543ef11ee
tibssy <norbert.tiborcz@gmail.com>
2025-04-05 18:48:04 +0100

    Create and display products model with image

--- products/models.py
+++ products/models.py
@@ -1,8 +1,11 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+
 
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = CloudinaryField('image', default='placeholder')
 
     def __str__(self):
         return self.name
