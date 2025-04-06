76c605005ee3e5c5978adb0c209a4484b6b43e02
tibssy <norbert.tiborcz@gmail.com>
2025-04-06 02:11:13 +0100

    Create category and update product models

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,21 @@
 from django.contrib import admin
-from .models import Product
+from .models import Category, Product
+from django.utils.html import format_html
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug', 'description')
+    search_fields = ('name', 'description')
+    prepopulated_fields = {'slug': ('name',)}
+
 
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
-    list_display = ('name', 'price')
+    list_display = ('name', 'price', 'created_on', 'updated_on', 'image_tag')
+    search_fields = ('name',)
+
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="60" />', obj.image.url)
+
+    image_tag.short_description = 'Image'
--- products/models.py
+++ products/models.py
@@ -2,10 +2,35 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+
+    class Meta:
+        verbose_name_plural = "Categories"
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     name = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     image = CloudinaryField('image', default='placeholder')
+    categories = models.ManyToManyField(Category, related_name='products')
+    description = models.TextField(default="")
+    sku = models.CharField(max_length=50, unique=True, blank=True, null=True, default=None)
+    stock_quantity = models.PositiveIntegerField(default=0)
+    is_featured = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created_on']
 
     def __str__(self):
         return self.name
