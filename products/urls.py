bf80b0003fcfc2df113e09f299bbc1ea4ab9a70b
tibssy <norbert.tiborcz@gmail.com>
2025-04-07 19:10:06 +0100

    Create product detail page and make product card modular

--- products/urls.py
+++ products/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.ProductListView.as_view(), name='products'),
+    path('', views.ProductListView.as_view(), name='product_list'),
+    path('product/<int:pk>/', views.ProductDetailView.as_view(), name='product_detail'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.views.generic import ListView
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 
@@ -8,4 +8,22 @@ class ProductListView(ListView):
     queryset = Product.objects.all().order_by("-created_on")
     template_name = 'products/product_list.html'
     context_object_name = 'products'
-    paginate_by = 4
+    paginate_by = 3
+
+
+class ProductDetailView(DetailView):
+    model = Product
+    template_name = 'products/product_detail.html'
+    context_object_name = 'product'
+
+
+    def get_object(self):
+        return get_object_or_404(Product, pk=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        product = self.get_object()
+        related_products = Product.objects.filter(categories__in=product.categories.all()).exclude(pk=product.pk).distinct()
+        context['related_products'] = related_products[:4]
+
+        return context
