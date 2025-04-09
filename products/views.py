79593b9b47fcd5ae039773ffd3d4d77c0baa7670
tibssy <norbert.tiborcz@gmail.com>
2025-04-09 20:11:59 +0100

    Add product search functionality

--- products/views.py
+++ products/views.py
@@ -1,14 +1,33 @@
 from django.views.generic import ListView, DetailView
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.db.models import Q
 
 
 class ProductListView(ListView):
     # model = Product
-    queryset = Product.objects.all().order_by("-created_on")
     template_name = 'products/product_list.html'
     context_object_name = 'products'
     paginate_by = 3
+
+    def get_queryset(self):
+        queryset = Product.objects.all().order_by("-created_on")
+        search_query = self.request.GET.get('q')
+
+        if search_query:
+            queryset = queryset.filter(
+                Q(name__icontains=search_query) |
+                Q(description__icontains=search_query) |
+                Q(sku__icontains=search_query) |
+                Q(categories__name__icontains=search_query)
+            ).distinct()
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['search_term'] = self.request.GET.get('q', '')
+        return context
 
 
 class ProductDetailView(DetailView):
