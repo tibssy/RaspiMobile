ecb1418029d0b73b2a15ee6fb8b2e2c85a56b9ac
tibssy <norbert.tiborcz@gmail.com>
2025-04-11 01:09:38 +0100

    Create cart app

--- raspimobile/settings.py
+++ raspimobile/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'crispy_bootstrap5',
     'home',
     'products',
+    'cart',
 ]
 
 # Crispy Form
@@ -109,16 +110,16 @@ WSGI_APPLICATION = "raspimobile.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.sqlite3",
-#         "NAME": BASE_DIR / "db.sqlite3",
-#     }
+#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
 
 
 CSRF_TRUSTED_ORIGINS = [
