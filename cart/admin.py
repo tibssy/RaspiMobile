cb873dc978d69c7afcadf5d4c529123efdad79bd
tibssy <norbert.tiborcz@gmail.com>
2025-04-11 01:32:35 +0100

    Create cart model to store cart content

--- cart/admin.py
+++ cart/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Cart, CartItem
 
-# Register your models here.
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user', 'created_on', 'updated_on']
+    list_filter = ['user', 'created_on', 'updated_on']
+    search_fields = ['user__username', 'id']
+    inlines = [CartItemInline]
+
+
+@admin.register(CartItem)
+class CartItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cart', 'product', 'quantity']
+    list_filter = ['cart', 'product']
+    search_fields = ['product__name', 'cart__id']
--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.conf import settings
+from products.models import Product
 
-# Create your models here.
+
+class Cart(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True, related_name='carts')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'Cart for {self.user if self.user else 'Guest'} - ID: {self.id}'
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return f'{self.quantity} x {self.product.name} in Cart {self.cart.id}'
+
+    def total_price(self):
+        return self.quantity * self.product.price
+
+
+    class Meta:
+        unique_together = ('cart', 'product')
