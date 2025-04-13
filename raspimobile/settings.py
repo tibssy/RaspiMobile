1a5fb289aad5bef47c420437cc4de7875d07b583
tibssy <norbert.tiborcz@gmail.com>
2025-04-13 19:36:43 +0100

    Switch back to PostgreSQL

--- raspimobile/settings.py
+++ raspimobile/settings.py
@@ -112,16 +112,16 @@ WSGI_APPLICATION = "raspimobile.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 CSRF_TRUSTED_ORIGINS = [
