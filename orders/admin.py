5d1ec765e4571a421e46a479f657cdc9b59f970c
tibssy <norbert.tiborcz@gmail.com>
2025-04-15 05:01:12 +0100

    Register models from orders app on admin panel

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,40 @@
 from django.contrib import admin
+from .models import Order, OrderItem, ShippingAddress, DeliveryMethod
 
-# Register your models here.
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'user', 'shipping_address', 'delivery_method', 'order_total', 'status', 'date_ordered']
+    list_filter = ['status', 'date_ordered', 'delivery_method']
+    search_fields = ['order_number', 'shipping_address__full_name', 'user__username', 'shipping_address__email']
+    readonly_fields = ['order_number', 'date_ordered', 'date_updated']
+    inlines = [OrderItemInline]
+    fieldsets = (
+        (None, {
+            'fields': ('order_number', 'user', 'shipping_address', 'delivery_method', 'status')
+        }),
+        ('Totals', {
+            'fields': ('order_total',)
+        }),
+        ('Dates', {
+            'fields': ('date_ordered', 'date_updated')
+        }),
+    )
+
+
+@admin.register(ShippingAddress)
+class ShippingAddressAdmin(admin.ModelAdmin):
+    list_display = ['full_name', 'email', 'address1', 'city', 'country']
+    search_fields = ['full_name', 'email', 'address1', 'city', 'zipcode']
+
+
+@admin.register(DeliveryMethod)
+class DeliveryMethodAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price', 'is_active']
+    list_filter = ['is_active']
+    search_fields = ['name', 'description']
