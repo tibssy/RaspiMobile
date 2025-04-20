4ad7dcdde335266b0577b0b543a9dce85b92f2f7
tibssy <norbert.tiborcz@gmail.com>
2025-04-20 02:35:04 +0100

    Style stripe element

--- static/js/payment.js
+++ static/js/payment.js
@@ -5,8 +5,67 @@ document.addEventListener('DOMContentLoaded', function() {
     const orderNumber = orderNumberElement ? orderNumberElement.dataset.orderNumber : null;
     const stripe = Stripe(stripePublicKey);
 
+    const primaryBackgroundVar = '#45c95d';
+    const secondaryColorVar = '#198754';
+    const dangerColorVar = '#dc3545';
+    const boxShadowNormalVar = '0 2px 4px #00000080';
+    const focusBackgroundColor = '#ffffff';
+
     const appearance = {
-        theme: 'stripe',
+        theme: 'flat',
+        variables: {
+            colorPrimary: secondaryColorVar,
+            colorBackground: '#fff',
+            focusBoxShadow: boxShadowNormalVar,
+
+        },
+        rules: {
+            '.Input': {
+                border: '1px solid transparent',
+                borderBottom: `1px solid ${primaryBackgroundVar}`,
+                borderRadius: '0px',
+                padding: '0.6rem 0.75rem',
+                transition: 'transform 0.3s ease-in-out, background-color 0.3s ease-in-out, border 0.3s ease-in-out, box-shadow 0.3s ease-in-out, border-radius 0.3s ease-in-out',
+                outline: 'none',
+            },
+            '.Input:focus': {
+                border: `1px solid ${primaryBackgroundVar}`,
+                borderRadius: '6px'
+            },
+            '.Input--invalid': {
+                border: '1px solid transparent',
+                borderBottom: `1px solid ${dangerColorVar}`,
+                borderRadius: '0px',
+                padding: '0.6rem 0.75rem',
+                outline: 'none',
+                boxShadow: 'none'
+            },
+            '.Input--invalid:focus': {
+                border: `1px solid ${dangerColorVar}`,
+                borderRadius: '6px'
+            },
+            '.Label': {
+                fontWeight: '500',
+                paddingLeft: '0.5rem',
+                paddingBottom: '0.5rem'
+            },
+            '.Tab': {
+                borderRadius: '6px',
+                border: '1px solid transparent',
+            },
+            '.Tab:hover': {
+                boxShadow: boxShadowNormalVar,
+                border: `1px solid ${primaryBackgroundVar}`,
+            },
+            '.Tab--selected': {
+                border: '1px solid transparent',
+                backgroundColor: secondaryColorVar,
+                color: '#fff',
+            },
+            '.u-color-primary': {
+                color: primaryBackgroundVar,
+            }
+        }
     };
     const elements = stripe.elements({ appearance, clientSecret });
 
